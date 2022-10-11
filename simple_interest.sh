dfb334fd1243152503652c1d4f650951b32b534d
kaveri2424 <112706741+kaveri2424@users.noreply.github.com>
2022-10-11 19:50:16 +0530

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,10 @@
+
+echo "Enter the Principle Amount: "
+read p
+echo "Enter the rate of interest: "
+read r
+echo "Enter the number of years: "
+read s
+i=`expr $p \* $r \* $s
+i=`expr $i / 100`
+echo "The Simple Interest is :Rs.$i"
